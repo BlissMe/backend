a0593f9fcbfc41e477945e6944d2c2c148578f0a
Imasha Nawodi <imashanaw1999@gmail,com>
2025-06-10 01:12:28 +0530

    update [ort]

--- passport.js
+++ passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:8000/auth/google/callback",
+      callbackURL: "http://localhost:8080/auth/google/callback",
       scope: ["profile", "email"],
     },
     async (accessToken, refreshToken, profile, done) => {
