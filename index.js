aa7467cdb946b5a56878003278b7ba4d2b34bd7e
supunijayasinghe <supuninavodya695@gmail.com>
2025-06-10 09:12:02 +0530

    connect to the postgreSQL database.

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 require('dotenv').config();
+const db = require('./src/config/db');
+    
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+
 const indexRoute = require('./src/routes/index.route');
-const bodyParser = require('body-parser');
+
+// Import DB connection (just importing ensures it runs)
 
 app.use(bodyParser.json());
-
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors({ origin: '*' }));
 app.use('', indexRoute);
 
 app.listen(port, () => {
-    console.log(`BlissMe APIs listening on port - ${port}`)
-})
+    console.log(`BlissMe APIs listening on port - ${port}`);
+});
