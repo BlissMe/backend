60db689e2bb6ed2bb73c31200fbc32a85d78cd88
Imasha Nawodi <imashanaw1999@gmail,com>
2025-06-11 07:29:44 +0530

    change encryption method

--- src/utils/encyption.js
+++ src/utils/encyption.js
@@ -1,24 +1,12 @@
 const CryptoJS = require("crypto-js");
-require("dotenv").config();
 
-const secretKey = process.env.ENCRYPTION_KEY;
-
-// Encrypt function
-function encryptText(text) {
-  return CryptoJS.AES.encrypt(text, secretKey).toString();
-}
-
-// Decrypt function
-function decryptText(cipherText) {
-  const bytes = CryptoJS.AES.decrypt(cipherText, secretKey);
-   try {
-    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-  } catch (error) {
-    return data;
+function encryptText(value) {
+  if (typeof value !== "string") {
+    value = JSON.stringify(value);
   }
+  return CryptoJS.SHA256(value.trim().toLowerCase()).toString(CryptoJS.enc.Hex);
 }
 
 module.exports = {
-  encryptText,
-  decryptText,
+  encryptText
 };
