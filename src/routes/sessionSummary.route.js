76ae0492c164eb29d08b017cb2e916062f0e8594
Piumi Kavindya <piyumikavindyappk@gmail.com>
2025-06-22 15:11:22 +0530

    send session summaries to llm

--- src/routes/sessionSummary.route.js
+++ src/routes/sessionSummary.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { handleGenerateSessionSummary } = require("../controllers/sessionSummaryController");
+const { handleGenerateSessionSummary, handleGetSessionSummaries } = require("../controllers/sessionSummaryController");
 const { authenticateToken } = require("../services/authentication");
 
 const router = express.Router();
 
 router.post("/generate-summary", authenticateToken, handleGenerateSessionSummary);
+router.get("/session-summaries", authenticateToken, handleGetSessionSummaries);
 
 module.exports = router;
