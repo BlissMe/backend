d91562f639bfbbabd449b90972ae50219571d40f
supunijayasinghe <supuninavodya695@gmail.com>
2025-06-27 12:42:32 +0530

    getPreferences

--- src/services/userService.js
+++ src/services/userService.js
@@ -6,11 +6,6 @@ const setInitialPreferences = async (userId, { nickname, virtualCharacter, input
     const user = await User.findOne({ userID: userId });
     if (!user) throw new Error("User not found");
 
-    console.log("Setting initial preferences for user:", userId);
-    console.log("Setting initial preferences for user:", user);
-    console.log("Setting initial preferences for user:", user.userId);
-    console.log("Raw nickname from DB:", user.nickname);
-
     if ((user.nickname ?? "").trim() !== "") {
         throw new Error("Preferences already set");
     }
@@ -29,21 +24,33 @@ const getUserPreferences = async (userId) => {
 
     return {
         nickname: user.nickname ? decrypt(user.nickname) : null,
-        virtualCharacter: user.virtualCharacter ? decrypt(user.virtualCharacter) : null,
-        inputMode: user.inputMode ? decrypt(user.inputMode) : null,
+        virtualCharacter: user.virtualCharacter || null,
+        inputMode: user.inputMode || null,
     };
 };
 
 const updateNickname = async (userId, nickname) => {
-    return await User.findByIdAndUpdate(userId, { nickname: encrypt(nickname) }, { new: true });
+    return await User.findOneAndUpdate(
+        { userID: userId },
+        { nickname: encrypt(nickname) },
+        { new: true }
+    );
 };
 
 const updateVirtualCharacter = async (userId, virtualCharacter) => {
-    return await User.findByIdAndUpdate(userId, { virtualCharacter: encrypt(virtualCharacter) }, { new: true });
+    return await User.findOneAndUpdate(
+        { userID: userId },
+        { virtualCharacter: virtualCharacter },
+        { new: true }
+    );
 };
 
 const updateInputMode = async (userId, inputMode) => {
-    return await User.findByIdAndUpdate(userId, { inputMode: encrypt(inputMode) }, { new: true });
+    return await User.findOneAndUpdate(
+        { userID: userId },
+        { inputMode: inputMode },
+        { new: true }
+    );
 };
 
 module.exports = {
