6f537d0908c356824a6734c2412522cb16105ffb
supunijayasinghe <supuninavodya695@gmail.com>
2025-06-27 13:24:32 +0530

    character uploading with cloudinary and multer

---
+++ src/services/characterService.js
@@ -0,0 +1,26 @@
+const cloudinary = require("../utils/cloudinary");
+const Character = require("../models/characterModel");
+const fs = require("fs");
+
+const uploadCharacterService = async (filePath, name) => {
+    // Upload image to Cloudinary
+    const result = await cloudinary.uploader.upload(filePath, {
+        folder: "virtual_characters"
+    });
+
+    const character = new Character({
+        name,
+        imageUrl: result.secure_url
+    });
+
+    await character.save();
+
+    // Remove temp file
+    fs.unlinkSync(filePath);
+
+    return character;
+};
+
+module.exports = {
+    uploadCharacterService
+};
