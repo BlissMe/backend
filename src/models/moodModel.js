8472de0797d931aa1762a708ffc517f4876d78e3
ImashaNawodi <imashanaw1999@gmail.com>
2025-09-15 16:10:49 +0530

    create mood is work now

---
+++ src/models/moodModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const moodRecordSchema = new mongoose.Schema(
+  {
+    userId: { 
+      type: Number,   
+      ref: "User",
+      required: true,
+    },
+    mood: {
+      type: String,
+      required: true,
+    },
+    sleepHours: {
+      type: String,
+      default: "",
+    },
+    reflection: {
+      type: String,
+      default: "",
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("MoodRecord", moodRecordSchema);
