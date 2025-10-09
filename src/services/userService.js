3d38ab6df5feb81da0d3a41c5c09d57b94f8e480
ImashaNawodi <imashanaw1999@gmail.com>
2025-10-10 00:15:45 +0530

    getall user detals api done

--- src/services/userService.js
+++ src/services/userService.js
@@ -82,6 +82,18 @@ const updateUserPreferences = async (userId, preferences) => {
     return updatedUser;
 };
 
+const getAllUserPreferences = async () => {
+ const users = await User.find({});
+    if (!users) throw new Error("Users not found");
+
+    const decryptedUsers = users.map(user => ({
+    ...user.toObject(), 
+    nickname: user.nickname ? decrypt(user.nickname) : null
+  }));
+
+  return { users: decryptedUsers };
+};
+
 module.exports = {
     setInitialPreferences,
     updateNickname,
@@ -89,4 +101,5 @@ module.exports = {
     updateInputMode,
     getUserPreferences,
     updateUserPreferences, 
+    getAllUserPreferences
 };
