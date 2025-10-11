cce99a3a70067ce15418a3fa985f6638245b6749
ImashaNawodi <imashanaw1999@gmail.com>
2025-10-11 23:36:22 +0530

    api updated

--- src/routes/phq9.route.js
+++ src/routes/phq9.route.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { authenticateToken } = require("../services/authentication");
-const { handleSavePHQ9Answer,handleGetAnsweredCountByUser } = require("../controllers/phq9Controller");
+const { handleSavePHQ9Answer,handleGetLastSession } = require("../controllers/phq9Controller");
 
 const router = express.Router();
 router.post("/phq9-save", authenticateToken, handleSavePHQ9Answer);
-router.get("/phq9/answered/:userID", authenticateToken, handleGetAnsweredCountByUser);
+router.get("/last-session/:userID", authenticateToken, handleGetLastSession);
 
 module.exports = router;
