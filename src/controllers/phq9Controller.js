fde477b87689eca2936292737b1755a23b422015
ImashaNawodi <imashanaw1999@gmail.com>
2025-10-11 23:03:03 +0530

    add new api

--- src/controllers/phq9Controller.js
+++ src/controllers/phq9Controller.js
@@ -1,4 +1,4 @@
-const { savePHQ9Answer } = require("../services/phq9Service");
+const { savePHQ9Answer, getAskedPHQ9IDs } = require("../services/phq9Service");
 
 const handleSavePHQ9Answer = async (req, res) => {
     const { userID } = req.user;
@@ -12,4 +12,28 @@ const handleSavePHQ9Answer = async (req, res) => {
     }
 };
 
-module.exports = { handleSavePHQ9Answer };
+const handleGetAnsweredCountByUser = async (req, res) => {
+  const { userID } = req.params;
+  try {
+    // find latest session for this user
+    const latestSession = await Session.findOne({ userID })
+      .sort({ createdAt: -1 }) // latest
+      .lean();
+
+    if (!latestSession) {
+      return res.json({ success: true, answeredCount: 0 });
+    }
+
+    const answeredIDs = await getAskedPHQ9IDs(userID, latestSession._id);
+    res.json({
+      success: true,
+      answeredCount: answeredIDs.length,
+    });
+  } catch (err) {
+    console.error("Error fetching answered count:", err);
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+
+
+module.exports = { handleSavePHQ9Answer,handleGetAnsweredCountByUser };
