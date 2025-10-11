62fea496baf7a52e2d479d46ff0962c02817a519
ImashaNawodi <imashanaw1999@gmail.com>
2025-10-11 23:36:22 +0530

    api updated

--- src/controllers/phq9Controller.js
+++ src/controllers/phq9Controller.js
@@ -1,39 +1,46 @@
-const { savePHQ9Answer, getAskedPHQ9IDs } = require("../services/phq9Service");
+const { savePHQ9Answer} = require("../services/phq9Service");
+const PHQ9Response = require("../models/phq9Model");
 
 const handleSavePHQ9Answer = async (req, res) => {
-    const { userID } = req.user;
-    const { sessionID, questionID, question, answer } = req.body;
-    try {
-        const saved = await savePHQ9Answer({ userID, sessionID, questionID, question, answer });
-        res.json({ success: true, saved });
-    } catch (err) {
-        console.error("Error saving PHQ9:", err);
-        res.status(500).json({ success: false, error: err.message });
-    }
-};
-
-const handleGetAnsweredCountByUser = async (req, res) => {
-  const { userID } = req.params;
+  const { userID } = req.user;
+  const { sessionID, questionID, question, answer } = req.body;
   try {
-    // find latest session for this user
-    const latestSession = await Session.findOne({ userID })
-      .sort({ createdAt: -1 }) // latest
-      .lean();
-
-    if (!latestSession) {
-      return res.json({ success: true, answeredCount: 0 });
-    }
-
-    const answeredIDs = await getAskedPHQ9IDs(userID, latestSession._id);
-    res.json({
-      success: true,
-      answeredCount: answeredIDs.length,
+    const saved = await savePHQ9Answer({
+      userID,
+      sessionID,
+      questionID,
+      question,
+      answer,
     });
+    res.json({ success: true, saved });
   } catch (err) {
-    console.error("Error fetching answered count:", err);
+    console.error("Error saving PHQ9:", err);
     res.status(500).json({ success: false, error: err.message });
   }
 };
 
+const handleGetLastSession = async (req, res) => {
+  const { userID } = req.params;
 
-module.exports = { handleSavePHQ9Answer,handleGetAnsweredCountByUser };
+  try {
+    const lastEntry = await PHQ9Response.findOne({ userID })
+      .sort({ createdAt: -1 }) // most recent session
+      .lean();
+
+    if (!lastEntry)
+      return res.json({ success: true, sessionID: null, answeredCount: 0 });
+
+    const { sessionID } = lastEntry;
+    const answeredCount = await PHQ9Response.countDocuments({
+      userID,
+      sessionID,
+    });
+
+    res.json({ success: true, sessionID, answeredCount });
+  } catch (err) {
+    console.error("Error getting last session:", err);
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+
+module.exports = { handleSavePHQ9Answer, handleGetLastSession };
