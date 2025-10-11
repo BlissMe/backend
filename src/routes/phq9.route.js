37536217f92344ec6b4726bb4768bee7bb5a516f
ImashaNawodi <imashanaw1999@gmail.com>
2025-10-11 23:03:03 +0530

    add new api

--- src/routes/phq9.route.js
+++ src/routes/phq9.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { authenticateToken } = require("../services/authentication");
-const { handleSavePHQ9Answer } = require("../controllers/phq9Controller");
+const { handleSavePHQ9Answer,handleGetAnsweredCountByUser } = require("../controllers/phq9Controller");
 
 const router = express.Router();
 router.post("/phq9-save", authenticateToken, handleSavePHQ9Answer);
+router.get("/phq9/answered/:userID", authenticateToken, handleGetAnsweredCountByUser);
+
 module.exports = router;
