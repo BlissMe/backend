f9d26550a50ff96474d2e7e0a4ca1db71e4feec8
ImashaNawodi <imashanaw1999@gmail.com>
2025-10-19 16:12:11 +0530

    check is done

--- doctor.js
+++ doctor.js
@@ -34,7 +34,7 @@ async function createDoctor() {
     });
 
     await doctor.save();
-    console.log("Doctor account created su!");
+    console.log("Doctor account created successfully!");
     process.exit(0);
   } catch (err) {
     console.error(err);
