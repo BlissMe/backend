09ea15142b371aaefe8d99a2fca8454c085d7804
ImashaNawodi <imashanaw1999@gmail.com>
2025-11-17 09:29:53 +0530

    fix nickname issue

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,4 @@
 const User = require("../models/userModel");
-const { encrypt, decrypt } = require("../utils/chatEncryption");
 
 const setInitialPreferences = async (userId, { nickname, virtualCharacter, inputMode }) => {
     const user = await User.findOne({ userID: userId });
@@ -9,10 +8,10 @@ const setInitialPreferences = async (userId, { nickname, virtualCharacter, input
         throw new Error("Preferences already set");
     }
 
-  const finalNickname = nickname && nickname.trim() !== "" ? nickname : "pinky";
-  user.nickname = encrypt(finalNickname);
-  user.virtualCharacter = virtualCharacter;
-  user.inputMode = inputMode;
+    const finalNickname = nickname && nickname.trim() !== "" ? nickname : "pinky";
+    user.nickname = finalNickname;
+    user.virtualCharacter = virtualCharacter;
+    user.inputMode = inputMode;
 
     return await user.save();
 };
@@ -23,19 +22,19 @@ const getUserPreferences = async (userId) => {
     if (!user) throw new Error("User not found");
 
     return {
-        nickname: user.nickname ? decrypt(user.nickname) : null,
+        nickname: user.nickname || null, 
         virtualCharacter: user.virtualCharacter || null,
         inputMode: user.inputMode || null,
     };
 };
 
 const updateNickname = async (userId, nickname) => {
-  const finalNickname = nickname && nickname.trim() !== "" ? nickname : "pinky";
-  return await User.findOneAndUpdate(
-    { userID: userId },
-    { nickname: encrypt(finalNickname) },
-    { new: true }
-  );
+    const finalNickname = nickname && nickname.trim() !== "" ? nickname : "pinky";
+    return await User.findOneAndUpdate(
+        { userID: userId },
+        { nickname: finalNickname },
+        { new: true }
+    );
 };
 
 const updateVirtualCharacter = async (userId, virtualCharacter) => {
@@ -58,7 +57,7 @@ const updateUserPreferences = async (userId, preferences) => {
     const updates = {};
 
     if (preferences.nickname !== undefined) {
-        updates.nickname = encrypt(preferences.nickname);
+        updates.nickname = preferences.nickname; 
     }
 
     if (preferences.virtualCharacter !== undefined) {
@@ -85,32 +84,32 @@ const updateUserPreferences = async (userId, preferences) => {
 };
 
 const getAllUserPreferences = async () => {
- const users = await User.find({});
+    const users = await User.find({});
     if (!users) throw new Error("Users not found");
 
-    const decryptedUsers = users.map(user => ({
-    ...user.toObject(), 
-    nickname: user.nickname ? decrypt(user.nickname) : null
-  }));
-  return { users: decryptedUsers };
+    // Removed decrypt
+    const plainUsers = users.map(user => ({
+        ...user.toObject(),
+        nickname: user.nickname || null
+    }));
+    return { users: plainUsers };
 };
 
 const updateDepressionLevel = async (userId, level) => {
-  return await User.findOneAndUpdate(
-    { userID: userId },
-    { depressionLevel: level },
-    { new: true }
-  );
+    return await User.findOneAndUpdate(
+        { userID: userId },
+        { depressionLevel: level },
+        { new: true }
+    );
 };
 
 const updateMedicineStatus = async (userId, status) => {
-  return await User.findOneAndUpdate(
-    { userID: userId },
-    { takesMedicine: status },
-    { new: true }
-  );
+    return await User.findOneAndUpdate(
+        { userID: userId },
+        { takesMedicine: status },
+        { new: true }
+    );
 };
-
 
 module.exports = {
     setInitialPreferences,
@@ -118,7 +117,7 @@ module.exports = {
     updateVirtualCharacter,
     updateInputMode,
     getUserPreferences,
-    updateUserPreferences, 
+    updateUserPreferences,
     getAllUserPreferences,
     updateDepressionLevel,
     updateMedicineStatus
