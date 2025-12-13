593eee2c3444bfabf191d7f43ce359d5dc7b1391
Piumi Kavindya <piyumikavindyappk@gmail.com>
2025-12-13 12:28:25 +0530

    store in schema1

--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { sendSubscription, sendSubscriberNotification ,requestOtp,verifyOtp} = require("../controllers/SubscrptionsContoller");
+const { sendSubscription, sendSubscriberNotification, requestOtp, verifyOtp } = require("../controllers/subscrptionsContoller");
 const router = express.Router();
+const authenticate = require("../middleware/authenticate");
 
 router.post("/subscribe", sendSubscription);
 router.post("/notify", sendSubscriberNotification);
 router.post("/otp-send", requestOtp);
-router.post("/otp-verify", verifyOtp);
+router.post("/otp-verify", authenticate, verifyOtp);
 
 
 module.exports = router;
