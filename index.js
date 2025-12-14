d74bb9b7ce96ed5d82c8439e609f55f84a4d8d2d
Piumi Kavindya <piyumikavindyappk@gmail.com>
2025-12-14 06:21:16 +0530

    com

--- index.js
+++ index.js
@@ -31,9 +31,12 @@ app.use(
   cors({
     origin: ["http://localhost:3000", "http://localhost:3001", "https://blissme.vercel.app", "https://doctor-portal-phi.vercel.app", "https://blissme-v2.vercel.app", "https://blissme-v1.vercel.app", "https://blissme-v3.vercel.app"],
     methods: "GET,POST,PUT,DELETE,PATCH",
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
+app.options("*", cors());
 
 app.use(
   session({
